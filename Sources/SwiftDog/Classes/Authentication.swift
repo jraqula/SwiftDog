0e708b40b0856a483947445074c81d6a0018526d
J Evelyn Quintana <q@world.oberlin.edu>
2018-06-30 13:03:11 -0700

    get credentials from env if necessary
    - helps with use cases where creds are in env rather than file,
      such as CI.
    - note that this is spiky and untested. If these experiments prove out,
      I would like to establish a CI setup & some testing.

--- Sources/SwiftDog/Classes/Authentication.swift
+++ Sources/SwiftDog/Classes/Authentication.swift
@@ -17,7 +17,25 @@ internal struct DatadogAuthentication {
     internal var api_key: String? {
         return self.keychain[string: "api_key"]
     }
-    
+
+    private mutating func get_credentials_from_env() throws {
+        guard let api_key = ProcessInfo.processInfo.environment["DATADOG_API_KEY"],
+              let app_key = ProcessInfo.processInfo.environment["DATADOG_APP_KEY"] else {
+            throw DatadogAPIError.keyNotSet("API OR APP KEY NOT SET")
+        }
+        if keychain["api_key"] == nil || keychain["app_key"] == nil {
+            do {
+                try keychain.set(api_key, key: "api_key")
+                try keychain.set(app_key, key: "app_key")
+                self.authenticated = true
+                print("finished setting credentials")
+            } catch {
+                throw DatadogAPIError.keyNotSet("Failed to set app or api key")
+            }
+
+        }
+    }
+
     private mutating func get_credentials_from_plist() throws {
         var myDict: NSDictionary?
         if let path = Bundle.main.url(forResource: "datadog_config", withExtension: "plist") {
@@ -45,7 +63,11 @@ internal struct DatadogAuthentication {
         do {
             try self.get_credentials_from_plist()
         } catch {
-            fatalError(error.localizedDescription)
+            do {
+                try self.get_credentials_from_env()
+            } catch {
+                fatalError(error.localizedDescription)
+            }
         }
     }
     
