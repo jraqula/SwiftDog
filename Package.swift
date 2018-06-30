a58aeed243e728f0dbf28ca1e58dbb998d73a6c4
J Evelyn Quintana <q@world.oberlin.edu>
2018-06-30 12:48:26 -0700

    must declare a library package to import via spm
    Swift libraries are the exported product from a package
    
    resolves this confusing error downstream:
    
    ```
    error: product dependency 'SwiftDog' not found
    warning: dependency 'SwiftDog' is not used by any target
    ```

--- Package.swift
+++ Package.swift
@@ -4,12 +4,18 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftDog",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "SwiftDog",
+            targets: ["SwiftDog"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", "3.0.0" ..< "4.0.0"),
     ],
     targets: [
         .target(
             name: "SwiftDog",
-            dependencies: ["KeychainAccess"])
+            dependencies: ["KeychainAccess"]),
     ]
 )
